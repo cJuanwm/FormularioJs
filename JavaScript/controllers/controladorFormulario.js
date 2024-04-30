e6dd70e90f23e82bf7ef3e3485d75f86d95856e9
Julian Lopera <lopjulian410@gmail.com>
2024-04-30 07:23:54 -0500

    Formulario Javascript

---
+++ JavaScript/controllers/controladorFormulario.js
@@ -0,0 +1,18 @@
+//Scope de las variables de JavaScript
+
+//Almacenando una etiqueta HTML en una variable de JS
+//Hago esto para poder controlar esa etiqueta desde la logica de JS
+
+let productNameBox = document.getElementById('nombreProducto');
+
+let productTipe = document.getElementById('tipoProducto');
+
+let productQuantity = document.getElementById('cantidadProducto');
+
+let price = document.getElementById('precio');
+
+let description = document.getElementById('descripcion');
+
+let termsConditions = document.getElementById('terminosCondicionesBox')
+
+let buttonId = document.getElementById('botonId');
