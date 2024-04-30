fa13eac495ce9c0c3ec2b4d71425b1d4317f937b
Julian Lopera <lopjulian410@gmail.com>
2024-04-30 11:29:51 -0500

    Nuevos cambios

--- JavaScript/controllers/controladorFormulario.js
+++ JavaScript/controllers/controladorFormulario.js
@@ -16,3 +16,17 @@ let description = document.getElementById('descripcion');
 let termsConditions = document.getElementById('terminosCondicionesBox')
 
 let buttonId = document.getElementById('botonId');
+
+//¿Como carajos detecto un evento en Js?
+//Paso 1. Una vez tenga una variable que almacena la etiqueta sobre la cual quiero escuchar el evento
+//Paso 2. Utilizar la funcion o metodo propio de Js watchdogs(Perro guardian)
+buttonId.addEventListener("click", function(evento){
+
+    evento.preventDefault;
+
+    //Logica que deseo activar si el evento se detecto con exito
+    console.log("Acabam de darle click a tu formulario")
+
+    //¿Como capturar el valor de un input de un formulario?
+
+})
